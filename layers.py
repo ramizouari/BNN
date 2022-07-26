5801fb0baa1cb20bcb4c0a9b29938a8619b8919b
ramizouari <zouari.rami@yahoo.com>
2022-07-26 01:13:07 +0100

    Adding multiple BNN models with experiments

--- layers.py
+++ layers.py
@@ -1,40 +1,192 @@
-import tensorflow as tf
+import tensorflow
 import larq
+import numpy
+
+"""
+    Convention: To avoid any unintentional naming conflict, we will add the prefix "db_" to every 
+    member variable name.
+"""
+
+
 
 """
 This is a simple normalisation layer for image data
+It will divide the pixel intensity of each channel by the max intensity
 """
-class ImageNormalisationLayer(tf.keras.layers.Layer):
+class ImageNormalisationLayer(tensorflow.keras.layers.Layer):
     def __init__(self,max_intensity=255):
         super(ImageNormalisationLayer, self).__init__()
         self.max_intensity=max_intensity
         
     def call(self,I):
         return I/self.max_intensity
-    
-
-
+    def get_config(self):
+        return {"max_intensity": self.max_intensity}
 
 
 """
-    This is a 2D convolutional binary layer scaled by factors ɑ and β as described by the paper
+    This is a dense binary layer scaled by factors ɑ and β as described by the paper.
 """
 
-class ScaledBinaryConv2D(larq.layers.QuantConv2D):
-    def __init__(self):
-        super(WeightedBinaryLayer,self).__init__(self)
+class ScaledQuantDense(larq.layers.QuantDense):
+    def __init__(self,units,alpha_trainable=False,train_K=False,*args,**kwargs):
+        if "activation" in kwargs:
+            activation=kwargs["activation"]
+            self.db_activation=activation
+        else:
+            self.db_activation=None
+        super(ScaledQuantDense,self).__init__(units,*args,**{k:kwargs[k] for k in kwargs if k!="activation"})
+        self.alpha_trainable=alpha_trainable
+        self.train_K=train_K
+
+    def build(self,input_shape):
+        super(ScaledQuantDense,self).build(input_shape)
+        self.db_dimension=input_shape[-1]
+        if self.alpha_trainable:
+            self.alpha=tensorflow.Variable(tensorflow.divide(tensorflow.norm(self.kernel,axis=0,ord=1),self.db_dimension),
+                               trainable=True)
+
+    def call(self,inputs,training=False):
+        #Result of Quantified dense layer
+        Z=super(ScaledQuantDense, self).call(inputs)
+        #Calculates the scale factor of the convolution kernel
+        alpha= self.alpha if self.alpha_trainable else \
+            tensorflow.divide(tensorflow.norm(self.kernel,axis=0,ord=1),self.db_dimension)
         
-    
-    def call(self,I):
-        pass
-    
-
-"""
-    This is a dense binary layer scaled by factors ɑ and β as described by the paper
-"""
-class ScaledDenseLayer(larq.layers.QuantDense):
+        #Calculates the scale factor of the input
+        beta=tensorflow.divide(tensorflow.norm(inputs,axis=-1,ord=1),self.db_dimension)
+        #Calculates the correction tensor
+        K=tensorflow.tensordot(beta,alpha,axes=0)
+        #Apply the correction tensor to the result point-wise
+        return tensorflow.multiply(Z, K)
+    def get_config(self):
+        config=super(ScaledQuantDense,self).get_config()
+        config.update({"alpha_trainable":self.alpha_trainable})
+        return config
     pass
 
+"""
+    This is a 2D convolutional binary layer scaled by factors ɑ and β as described by the paper.
+    It will first calculate a Quantified 2D convolution
+"""
 
 
+class ScaledQuantConv2D(larq.layers.QuantConv2D):
+    def __init__(self,filters,kernel_size,alpha_trainable=False,*args,**kwargs):
+        super(ScaledQuantConv2D,self).__init__(filters,kernel_size,*args,**kwargs)
+        self.alpha_trainable=alpha_trainable
+
+    def build(self,input_shape):
+        super(ScaledQuantConv2D,self).build(input_shape)
+        self.db_img_width=input_shape[1]
+        self.db_img_hight=input_shape[2]
+        self.db_img_channels=input_shape[3]
+        self.db_dimension=numpy.prod(self.kernel_size)*self.db_img_channels
+        self.db_ones_tensor=numpy.ones(list(self.kernel_size)+[input_shape[-1],1])
+        if self.alpha_trainable:
+            self.alpha=tensorflow.Variable(tensorflow.divide(
+                    tensorflow.reduce_sum(tensorflow.abs(self.kernel),axis=(0,1,2)),self.db_dimension),
+                                   trainable=True)
+
+    def call(self,inputs,training=False):
+        #Result of Binarised dense layer
+        Z=super(ScaledQuantConv2D, self).call(inputs)
+        #print(f"x:{self.kernel.shape}\t y:{inputs.shape}")
+        alpha=self.alpha if self.alpha_trainable else \
+            tensorflow.divide(
+                tensorflow.reduce_sum(tensorflow.abs(self.kernel),axis=(0,1,2)),self.db_dimension)
+        I=tensorflow.abs(inputs)
+        beta=tensorflow.divide(tensorflow.nn.conv2d(
+            I,self.db_ones_tensor,self.strides,self.padding.upper()),
+            self.db_dimension)
+        beta=beta[:,:,0]
+        #Adding scale factors
+        #print(f"a:{alpha.shape}\t b:{beta.shape}")
+        K=tensorflow.tensordot(beta,alpha,axes=0)
+        R=tensorflow.multiply(Z, K)
+        return R if not self.db_activation else self.db_activation(R)
+    def get_config(self):
+        config=super(ScaledQuantConv2D,self).get_config()
+        config.update({"alpha_trainable":self.alpha_trainable})
+        return config
+
+"""
+    This is a 1D convolutional binary layer scaled by factors ɑ and β as described by the paper.
+    It will first calculate a Quantified 1D convolution
+"""    
+    
+class ScaledQuantConv1D(larq.layers.QuantConv1D):
+    def __init__(self,*args,**kwargs):
+        super(ScaledQuantConv1D,self).__init__(*args,**kwargs)
+
+    def build(self,input_shape):
+        super(ScaledQuantConv1D,self).build(input_shape)
+        self.db_dimension=numpy.prod(input_shape[1:])
+        self.db_series_span=input_shape[1]
+        self.db_series_channels=input_shape[2]
+        self.db_ones_tensor=numpy.ones(list(self.kernel_size)+[input_shape[-1],1])
+
+    def call(self,inputs,training=False):
+        #Result of Binarised dense layer
+        Z=super(ScaledQuantConv1D, self).call(inputs)
+        alpha=tensorflow.divide(
+            tensorflow.reduce_sum(tensorflow.abs(self.kernel),axis=(0,1)),self.db_dimension)
+        I=tensorflow.abs(inputs)
+        beta=tensorflow.divide(tensorflow.nn.conv1d(
+            I,self.db_ones_tensor,self.strides,self.padding.upper()),
+            self.db_dimension)
+        beta=beta[:,0]
+        #Adding scale factors
+        K=tensorflow.tensordot(beta,alpha,axes=0)
+        return tensorflow.multiply(Z, K)
+    def get_config(self):
+        return super(ScaledQuantConv1D,self).get_config()
+    
+    
+class ScaledQuantConv3D(larq.layers.QuantConv3D):
+    def __init__(self,*args,**kwargs):
+        super(ScaledQuantConv3D,self).__init__(*args,**kwargs)
+
+    def build(self,input_shape):
+        super(ScaledQuantConv3D,self).build(input_shape)
+        self.db_width=input_shape[1]
+        self.db_hight=input_shape[2]
+        self.db_depth=input_shape[3]
+        self.db_channels=input_shape[4]
+        self.db_dimension=numpy.prod(self.kernel_size)*self.db_channels
+        self.db_ones_tensor=numpy.ones(list(self.kernel_size)+[input_shape[-1],1])
+
+    def call(self,inputs,training=False):
+        #Result of Binarised dense layer
+        Z=super(ScaledQuantConv3D, self).call(inputs)
+        alpha=tensorflow.divide(tensorflow.reduce_sum(tensorflow.abs(self.kernel),axis=(0,1,2,3)),self.db_dimension)
+        I=tensorflow.abs(inputs)
+        beta=tensorflow.divide(tensorflow.nn.conv3d(
+            I,self.db_ones_tensor,self.strides,self.padding.upper()),
+            self.db_dimension)
+        beta=beta[:,:,:,0]
+        #Adding scale factors
+        K=tensorflow.tensordot(beta,alpha,axes=0)
+        return tensorflow.multiply(Z, K)
+    def get_config(self):
+        return super(ScaledQuantConv3D,self).get_config()
+
+
+
+class ABCDense(tensorflow.keras.layers.Layer):
+    def __init__(self,units,kernel_estimators,activation_estimators,kernel_initializer="random_uniform",
+                 activation_initialize="random_uniform",*args,**kwargs):
+        super(tensorflow.keras.layers.Layer,self).__init__(*args,**kwargs)
+        self.kernel_estimators=kernel_estimators
+        self.activation_estimators=activation_estimators
+        self.units=units
+    pass
+    
+    def build(self,input_shape):
+        #self.kernels=tensorflow.Variable()
+        pass
+
+
+class ABCConv2D(tensorflow.keras.layers.Layer):
+    pass
 
--- model.py
+++ model.py
@@ -3,7 +3,7 @@ from tensorflow import keras
 
 import load
 import larq as lq
-
+import numpy as np
 from tensorflow.keras import optimizers,losses
 
 import layers
@@ -11,18 +11,22 @@ import layers
 
 data_format="channels_last"
 
-X_train,y_train=load.load_cifar("dataset/cifar-10","data*",data_format=data_format)
-y_train=tf.one_hot(y_train,10)
-X_test,y_test=load.load_cifar("dataset/cifar-10","test_batch",data_format=data_format)
 
+(X_train,y_train),(X_test,y_test)=tf.keras.datasets.mnist.load_data()
 quantization=dict(input_quantizer="ste_sign",
               kernel_quantizer="ste_sign",
               kernel_constraint="weight_clip",
               use_bias=False)
+y_train=tf.one_hot(y_train, 10)
+y_test=tf.one_hot(y_test, 10)
+X_train=X_train.astype(dtype=np.float32)
+X_test=X_test.reshape(list(X_test.shape)+[1])
+X_train=X_train.reshape(list(X_train.shape)+[1])
 
 
 cnn_model=tf.keras.Sequential([
-    tf.keras.layers.Conv2D(192,kernel_size=(5,5),activation="relu",padding="same", data_format=data_format),
+    tf.keras.layers.Conv2D(192,kernel_size=(5,5),input_shape=(32,32,1),
+                           activation="relu",padding="same", data_format=data_format),
     tf.keras.layers.Conv2D(64,kernel_size=(1,1),activation="relu",padding="same", data_format=data_format),
     tf.keras.layers.MaxPooling2D(pool_size=(2,2),strides=2, data_format=data_format),
    # lq.layers.QuantConv2D(192, kernel_size=(1,1), input_quantizer="ste_sign",
@@ -40,48 +44,54 @@ cnn_model=tf.keras.Sequential([
     
     ])
 
+
+Conv2D=layers.ScaledQuantConv2D
+Dense=layers.ScaledQuantDense
+
+
 bnn_model=tf.keras.Sequential([
-    tf.keras.layers.GaussianNoise(stddev=4),
+    tf.keras.layers.GaussianNoise(stddev=4,input_shape=(28,28,1)),
     layers.ImageNormalisationLayer(),
     tf.keras.layers.RandomCrop(width=28,height=28),
     tf.keras.layers.Conv2D(192, 3,
-                          use_bias=False,
-                          input_shape=(3, 32, 32)),
+                          use_bias=False),
     tf.keras.layers.ReLU(),
     tf.keras.layers.BatchNormalization(momentum=0.1, scale=False),
 
-    lq.layers.QuantConv2D(160, 1, padding="valid",data_format=data_format,**quantization),
-    lq.layers.QuantConv2D(92, 1, padding="valid",data_format=data_format,**quantization),
+    Conv2D(160, 1, padding="same",data_format=data_format,**quantization),
+    tf.keras.layers.ReLU(),
+    Conv2D(92, 1, padding="same",data_format=data_format,**quantization),
+    tf.keras.layers.ReLU(),
     tf.keras.layers.MaxPooling2D(pool_size=(3, 3), strides=(2, 2)),
     tf.keras.layers.BatchNormalization(momentum=0.1, scale=False),
     
 
-    lq.layers.QuantConv2D(192, 5, padding="same",data_format=data_format,**quantization),
-    tf.keras.layers.Dropout(0.3),
-    lq.layers.QuantConv2D(160, 1, padding="valid",data_format=data_format,**quantization),
+    Conv2D(192, 5, padding="same",data_format=data_format,**quantization),
+#    tf.keras.layers.Dropout(0.3),
+    Conv2D(160, 1, padding="same",data_format=data_format,**quantization),
     tf.keras.layers.ReLU(),
     tf.keras.layers.AveragePooling2D(pool_size=(3,3),strides=(2,2)),
 
     tf.keras.layers.BatchNormalization(momentum=0.1, scale=False),
 
-    lq.layers.QuantConv2D(72, 3, padding="same",**quantization),
+    Conv2D(72, 3, padding="same",**quantization),
     tf.keras.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2),data_format=data_format),
 
-    lq.layers.QuantConv2D(72, 3, padding="same",data_format=data_format,**quantization),
-    lq.layers.QuantConv2D(72, 1, padding="same",data_format=data_format,**quantization),
+    Conv2D(72, 3, padding="same",data_format=data_format,**quantization),
+    Conv2D(72, 1, padding="same",data_format=data_format,**quantization),
     tf.keras.layers.BatchNormalization(momentum=0.1, scale=False),
     tf.keras.layers.ReLU(),
 
 
-    tf.keras.layers.Dropout(0.3),
+    #tf.keras.layers.Dropout(0.3),
 
     tf.keras.layers.Flatten(),
-
-    lq.layers.QuantDense(128,**quantization),
     tf.keras.layers.BatchNormalization(momentum=0.1, scale=False),
 
-    lq.layers.QuantDense(128,**quantization),
+    Dense(128,**quantization),
     tf.keras.layers.BatchNormalization(momentum=0.1, scale=False),
+
+    Dense(128,**quantization),
 
     tf.keras.layers.Dense(10,activation="softmax")
     
@@ -90,4 +100,4 @@ bnn_model=tf.keras.Sequential([
 optimiser=optimizers.Adam(learning_rate=0.01,decay=0.00001)
 loss=losses.CategoricalCrossentropy(from_logits=False)
 bnn_model.compile(optimizer=optimiser,loss=loss,metrics=["accuracy"])
-bnn_model.fit(X_train,y_train,epochs=40,batch_size=128,validation_data=(X_test,tf.one_hot(y_test,10)))
+bnn_model.fit(X_train,y_train,epochs=40,batch_size=128,validation_data=(X_test,y_test))
